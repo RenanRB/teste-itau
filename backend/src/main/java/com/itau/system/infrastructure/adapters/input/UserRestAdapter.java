713e715b358872cfc0f43c81c6bd8b8490b71402
Renan <renan10r_@hotmail.com>
2023-10-21 14:53:06 +0100

    update cors

--- backend/src/main/java/com/itau/system/infrastructure/adapters/input/UserRestAdapter.java
+++ backend/src/main/java/com/itau/system/infrastructure/adapters/input/UserRestAdapter.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200", "kafka:4200"}, maxAge = 3600)
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @Slf4j
